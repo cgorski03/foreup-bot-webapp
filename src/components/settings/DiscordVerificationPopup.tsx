116d60cf453d3ca8ce59e0e1401ddc8081e531e5
Colin Gorski <cgorski03@gmail.com>
2024-04-18 14:32:52 -0400

    verification component working somewhat better not completely fantastic

--- src/components/settings/DiscordVerificationPopup.tsx
+++ src/components/settings/DiscordVerificationPopup.tsx
@@ -3,15 +3,53 @@ import './discordVerificationPopup.css';
 import { FaTimes } from 'react-icons/fa';
 import IconLabeledButton from '../buttons/IconLabeledButton';
 import { useCreateVerificationCode } from '../../utils/api/requests';
+import OutlinedButtonLoader from '../buttons/OutlinedButtonLoader';
+
+function VerificationCodeChar(props: { char: string }) {
+  const { char } = props;
+  return (
+    <div className="verificationCodeCharContainer">
+      <p>{char}</p>
+      <div className="verifyCharacterBase" />
+    </div>
+  );
+}
+
+type VerificationCodeDisplayProps = {
+  verificationCode: string;
+};
+
+function VerificationCodeDisplay(props: VerificationCodeDisplayProps) {
+  const { verificationCode } = props;
+  // Create an array of 5 empty characters so it will show blank
+  let codeArray: string[] = ['', '', '', '', ''];
+  // Check if there is a verification code
+  if (verificationCode.length !== 0) {
+    // Split the verification code into an array of characters
+    codeArray = verificationCode.split('');
+  }
+  return (
+    <div className="verificationCodeContainer">
+      {codeArray.map((char) => (
+        <VerificationCodeChar
+          key={Math.random()}
+          char={char}
+        />
+      ))}
+    </div>
+  );
+}
 
 type DiscordVerificationPopupProps = {
   closeWindow: () => void;
 };
+
 export default function DiscordVerificationPopup({
   closeWindow,
 }: DiscordVerificationPopupProps) {
   const [displayedVerificationCode, setDisplayedVerificationCode] = useState('');
-  const { createVerificationCode, verificationCode } = useCreateVerificationCode();
+  const { createVerificationCode, verificationCode, verificationCodeLoading } =
+    useCreateVerificationCode();
   const handleGenerateVerificationCode = () => {
     createVerificationCode();
   };
@@ -36,22 +74,26 @@ export default function DiscordVerificationPopup({
             href="https://discord.gg/HcSbFT2tzM"
             target="_blank"
             rel="noopener noreferrer"
+            aria-label="Join Discord Server"
           >
-            <button type="button">Join</button>
+            <OutlinedButtonLoader
+              onClick={() => {}}
+              loading={false}
+              buttonText="Join"
+              classOverride="connectDiscordButton"
+            />
           </a>
           <p>
             Then, send direct message to the bot using the /verify command with your
             verification code.
           </p>
-          <button
-            type="button"
+          <OutlinedButtonLoader
             onClick={handleGenerateVerificationCode}
-          >
-            Generate Verification Code
-          </button>
-          <div className="verificationCode">
-            <p>{displayedVerificationCode}</p>
-          </div>
+            loading={verificationCodeLoading}
+            buttonText="Generate Verification Code"
+            classOverride="connectDiscordButton"
+          />
+          <VerificationCodeDisplay verificationCode={displayedVerificationCode} />
         </div>
       </div>
     </div>
