888dab4d5e8af66953d2682d849977be01d68f6d
Colin Gorski <cgorski03@gmail.com>
2024-05-10 15:52:05 -0400

    Making the settings page less ugly and including the google form

--- src/components/settings/UserContactInformationField.tsx
+++ src/components/settings/UserContactInformationField.tsx
@@ -3,14 +3,13 @@ import { FaDiscord } from 'react-icons/fa';
 import { UserInformationContext } from '../../Contexts/UserContext';
 import './userContactInformation.css';
 import { useTestMessage } from '../../utils/api/requests';
-// @ts-ignore
-import { ReactComponent as Loader } from '../dashboard/SearchCard/spinner.svg';
 import DiscordVerificationPopup from './DiscordVerificationPopup';
 
 function UserContactInformationField() {
   const { userInfo } = useContext(UserInformationContext);
   const { sendTestMessage, testMessageLoading } = useTestMessage();
   const [showVerifyPopup, setShowVerifyPopup] = useState(false);
+
   const handleConnectDiscordButton = () => {
     setShowVerifyPopup(true);
   };
@@ -22,16 +21,13 @@ function UserContactInformationField() {
   return userInfo?.channel_id ? (
     <div className="settingsFieldContainer">
       <span className="settingLabel"> Connected contact method:</span>
-      <div className="settingValueContainer">
-        <span className="settingValueIcon">
-          <FaDiscord />
-        </span>
+      <div className="testMessageContainer">
         <button
           className={`testMessageButton ${testMessageLoading ? 'loading' : ''}`}
           onClick={handleTestMessageButton}
           type="submit"
         >
-          {testMessageLoading ? <Loader /> : 'Test'}
+          <FaDiscord className="settingValueIcon" /> <span>Test</span>
         </button>
       </div>
     </div>
@@ -44,7 +40,7 @@ function UserContactInformationField() {
           onClick={handleConnectDiscordButton}
           type="submit"
         >
-          {testMessageLoading ? <Loader /> : 'Connect Discord'}
+          <FaDiscord className="settingValueIcon" /> <span>Connect Discord</span>
         </button>
         {showVerifyPopup && (
           <DiscordVerificationPopup closeWindow={() => setShowVerifyPopup(false)} />
---
+++ src/pages/settings/CreatorMessage.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './creatorMessage.css';
+import { FaGithub, FaGooglePlusG } from 'react-icons/fa';
+
+export default function CreatorMessage() {
+  return (
+    <div className="creatorMessageContainer">
+      <h1 className="messageHeading">
+        <span className="handWave">👋</span> Hope you&apos;re having fun!
+      </h1>
+      <p className="messageText">
+        To report bugs, don&apos;t. They are actually secret easter eggs.
+      </p>
+      <p className="messageText">
+        If you insist on spoiling the fun, you can use the form below or brave GitHub to
+        submit an issue.
+      </p>
+      <div className="linkContainer">
+        <a
+          href="https://github.com/cgorski03"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub link"
+          className="githubLink"
+        >
+          <FaGithub className="githubIcon" />
+          <span className="githubText">Venture into GitHub</span>
+        </a>
+        <a
+          href="https://forms.google.com/your-form-url"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Google Form link"
+          className="formLink"
+        >
+          <FaGooglePlusG className="formIcon" />
+          <span className="formText">Report via Google Form</span>
+        </a>
+      </div>
+    </div>
+  );
+}
